a7ad9a0a2ddacaee9d99005e50e04b9f9da362c4
aaron-guensch <159801683+aaron-guensch@users.noreply.github.com>
2024-02-20 21:43:50 -0500

    conditions.py

---
+++ conditions.py
@@ -0,0 +1,18 @@
+# CS104
+# Aaron Guensch
+# conditions.py
+
+
+temp = int(input("Please enter the current temperature: "))
+
+#Providing clothing advice for different weather temperatures
+if temp > 90:
+    print("Wear Shorts")
+elif temp > 70:
+    print("Short sleeves are fine")
+elif temp > 50:
+    print("Wear a jacket")
+elif temp > 32:
+    print("Wear a heavy coat")
+else:
+    print("Stay Inside")
